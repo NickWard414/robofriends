57006f2ba3d8e259f110aa74aed89447f87f465e
NickWard414 <nward414@gmail.com>
2019-08-17 19:58:24 -0500

    Updates

---
+++ precache-manifest.31ac707f2bc569c2333aeb52c2097185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d61f83b979b166f8327b",
+    "url": "/robofriends/static/css/main.b32a5089.chunk.css"
+  },
+  {
+    "revision": "d61f83b979b166f8327b",
+    "url": "/robofriends/static/js/main.d61f83b9.chunk.js"
+  },
+  {
+    "revision": "f35cd20dc4da50a92aeb",
+    "url": "/robofriends/static/css/1.ae87dfdb.chunk.css"
+  },
+  {
+    "revision": "f35cd20dc4da50a92aeb",
+    "url": "/robofriends/static/js/1.f35cd20d.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "03ba510233f2857694a2155c25753f66",
+    "url": "/robofriends/index.html"
+  }
+];
